362bb3ebe7083775346d92aab218d2822c108827
Oscar Fernandéz Jimenéz <oscar_es46@outlook.com>
2025-02-24 20:43:44 -0600

    Change skills

--- portfolio/src/components/About/index.js
+++ portfolio/src/components/About/index.js
@@ -1,12 +1,12 @@
-import { useEffect, useState } from 'react'
-import {faAngular, faCss3, faGitAlt, faHtml5, faJsSquare, faReact,} from '@fortawesome/free-brands-svg-icons'
+import { useState } from 'react'
+import { faPython, faAngular, faCss3Alt, faGitAlt, faHtml5, faJsSquare, faReact, faDocker } from '@fortawesome/free-brands-svg-icons'
 import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './index.scss'
 
 const About = () => {
-  const [letterClass, setLetterClass] = useState('text-animate')
+  const [letterClass] = useState('text-animate')
   
   return (
     <>
@@ -20,42 +20,50 @@ const About = () => {
             />
           </h1>
           <p>
-            I'm a very ambitious front-end developer looking for a role in an
-            established IT company with the opportunity to work with the latest
-            technologies on challenging and diverse projects.
+            I'm a highly motivated Computer Science graduate looking for a role in an established IT company, 
+            where I can work with the latest technologies on challenging and diverse projects. With expertise in Python, 
+            C++, and C#, I aim to bring innovative solutions and contribute to the success of the team.
           </p>
           <p align="LEFT">
-            I'm quiet confident, naturally curious, and perpetually working on
-            improving my chops one design problem at a time.
-          </p>
-          <p>
-            If I need to define myself in one sentence that would be a family
-            person, father of a beautiful daughter, a sports fanatic,
-            photography enthusiast, and tech-obsessed!!!
+            I'm quiet confident, naturally curious, and always working on improving my skills one technical challenge 
+            at a time. Whether it's front-end, back-end, or full-stack development, I thrive on building high-quality, 
+            efficient solutions and collaborating in Agile environments.
           </p>
         </div>
 
-        <div className="stage-cube-cont">
-          <div className="cubespinner">
-            <div className="face1">
-              <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+        <div className="skills">
+            <div className="skill">
+              <FontAwesomeIcon icon={faPython} color="#FFD43B" />
+              <p className="skill-name">Python</p>
             </div>
-            <div className="face2">
+            <div className="skill">
               <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+              <p className="skill-name">HTML5</p>
             </div>
-            <div className="face3">
-              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            <div className="skill">
+              <FontAwesomeIcon icon={faCss3Alt} color="#039BE5" />
+              <p className="skill-name">CSS3</p>
             </div>
-            <div className="face4">
-              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            <div className="skill">
+              <FontAwesomeIcon icon={faReact} color="#61DBFB" />
+              <p className="skill-name">React</p>
             </div>
-            <div className="face5">
+            <div className="skill">
+              <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+              <p className="skill-name">Angular</p>
+            </div>
+            <div className="skill">
               <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+              <p className="skill-name">JavaScript</p>
             </div>
-            <div className="face6">
+            <div className="skill">
               <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+              <p className="skill-name">Git</p>
             </div>
-          </div>
+            <div className="skill">
+              <FontAwesomeIcon icon={faDocker} color="#0db7ed" />
+              <p className="skill-name">Docker</p>
+            </div>
         </div>
       </div>
       <Loader type="ball-scale-ripple" />
