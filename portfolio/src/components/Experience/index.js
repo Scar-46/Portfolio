dfdaef1415f6f6ef80248b01f9804dd7f616679f
Oscar Fernandéz Jimenéz <oscar_es46@outlook.com>
2025-03-07 23:40:53 -0600

    Added current date

--- portfolio/src/components/Experience/index.js
+++ portfolio/src/components/Experience/index.js
@@ -1,84 +1,65 @@
 import React, { useEffect, useState } from "react";
-import AnimatedLetters from '../AnimatedLetters'
-import './index.scss'
+import AnimatedLetters from "../AnimatedLetters";
+import experiencesData from "../../data/experiences.json";
+import "./index.scss";
 
 const Experience = () => {
-  const [letterClass, setLetterClass] = useState('text-animate');
+  const [letterClass, setLetterClass] = useState("text-animate");
+  const [hoveredImage, setHoveredImage] = useState(experiencesData[0].image);
 
   useEffect(() => {
-      const timer = setTimeout(() => {
-          setLetterClass('text-animate-hover');
-      }, 4000);
-
-      return () => clearTimeout(timer);
+    const timer = setTimeout(() => {
+      setLetterClass("text-animate-hover");
+    }, 4000);
+    return () => clearTimeout(timer);
   }, []);
-  
+
   return (
-    <>
-      <div className="container experience-page">
-        <div className="text-zone">
-          <h1>
-            <AnimatedLetters
-              letterClass={letterClass}
-              strArray={['E', 'x', 'p', 'e', 'r', 'i', 'e', 'n', 'c', 'e']}
-              idx={15}
-            />
-          </h1>
+    <div className="container experience-page">
+      <div className="text-zone">
+        <h1>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={["E", "x", "p", "e", "r", "i", "e", "n", "c", "e"]}
+            idx={15}
+          />
+        </h1>
+      </div>
+
+      <div className="experience-content">
+        <div className="experiences">
+          {experiencesData.map((exp, index) => (
+            <div
+              key={index}
+              className="experience"
+              onMouseEnter={() => setHoveredImage(exp.image)}
+            >
+              <div className="experience-header">
+                <h2 className="title">{exp.title}</h2>
+                <h4 className="date">{exp.date}</h4>
+              </div>
+              <h3 className="company">{exp.company}</h3>
+              <ul>
+                {exp.responsibilities.map((task, i) => (
+                  <li key={i}>{task}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
         </div>
 
-        <div className="experience-content">
-          <div className="experiences">
-          <div className="experience">
-              <div className="experience-header">
-                <h2 className='title'>Data Analist - Community Service</h2>
-                <h4 className='date'>Nov 2023 - Nov 2024</h4>
-              </div>
-              <h3 className='company'>Universidad de Costa Rica</h3>
-              <ul>
-                <li>Processed student performance data to assess lesson effectiveness.</li>
-                <li>Developed data-driven materials, increasing engagement by 25%.</li>
-                <li>Used statistical analysis and visualization to identify trends.</li>
-              </ul>
-            </div>
-            <div className="experience">
-              <div className="experience-header">
-                <h2 className='title'>Technical Support Specialist - Intership</h2>
-                <h4 className='date'>Oct 2020 - Dec 2020</h4>
-              </div>
-              <h3 className='company'>Robert Bosch GmbH</h3>
-              <ul>
-                <li>Diagnosed and resolved 100+ camera-related technical issues, ensuring 95%+ optimal functionality.</li>
-                <li>Provided real-time technical support to 50+ customers via phone, reducing resolution times by 10%.</li>
-                <li>Received specialized training in CCTV systems, enhancing expertise in security camera troubleshooting.</li>
-              </ul>
-            </div>
-            <div className="experience">
-              <div className="experience-header">
-                <h2 className='title'>Technical Support Specialist - Intership</h2>
-                <h4 className='date'>Sep 2019 - Oct 2019</h4>
-              </div>
-              <h3 className='company'>Servicios Ecológicos M.B.B. S.A </h3>
-              <ul>
-                <li>Developed and maintained relational and non-relational databases, improving data accessibility.</li>
-                <li>Performed system maintenance and optimization, enhancing overall computer performance.</li>
-                <li>Designed and implemented network topologies to improve communication and efficiency.</li>
-              </ul>
-            </div>
-          </div>
-
-          <div className="journey-dots">
+        <div className="journey-dots">
             <span className="dot-ex active"></span>
             <span className="dot-ex"></span>
             <span className="dot-ex"></span>
           </div>
 
-          <div className="experience-image">
-            <img src="your-image-url" alt="Experience" />
-          </div>
+        <div className="experience-image">
+          <img src={hoveredImage} alt="Experience" />
         </div>
       </div>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default Experience
+export default Experience;
--- portfolio/src/components/Navbar/index.js
+++ portfolio/src/components/Navbar/index.js
@@ -7,6 +7,9 @@ import { useState, useEffect } from 'react';
 const Navbar = () => {
   const [showNav, setShowNav] = useState(false);
   const [currentSection, setCurrentSection] = useState('home');
+  const [currentDate, setCurrentDate] = useState('');
+  const [isAtBottom, setIsAtBottom] = useState(false);
+
 
   useEffect(() => {
     const handleScroll = () => {
@@ -14,7 +17,7 @@ const Navbar = () => {
       let newSection = 'home';
       sectionEls.forEach((sectionEl) => {
         let offsetT = sectionEl.offsetTop;
-        if ((window.scrollY) >= offsetT - 300) {
+        if (window.scrollY >= offsetT - 300) {
           newSection = sectionEl.id;
         }
       });
@@ -24,17 +27,38 @@ const Navbar = () => {
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const isBottom = window.innerHeight + window.scrollY >= document.body.scrollHeight;
+      setIsAtBottom(isBottom);
+    };
+  
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  useEffect(() => {
+    const formatDate = (date) => {
+      const options = { month: 'short', day: '2-digit', year: 'numeric' };
+      const formattedDate = date.toLocaleDateString('en-US', options);
+      const parts = formattedDate.split(' ');
+      return `${parts[0]} ${parts[1].replace(',', '')}, ${parts[2]}`;
+    };
+  
+    setCurrentDate(formatDate(new Date()));
+  }, []);
+
   return (
     <div className="nav-bar">
       <nav className={showNav ? 'mobile-show' : ''}>
-      <Link to="home" smooth={true} duration={500} className={`home-link ${currentSection === 'home' ? 'active' : ''}`} onClick={() => setShowNav(false)}>
+        <Link to="home" smooth={true} duration={500} className={`home-link ${currentSection === 'home' ? 'active' : ''}`} onClick={() => setShowNav(false)}>
           <FontAwesomeIcon icon={faHome} color="var(--secondary-color)" />
         </Link>
         <Link to="about" smooth={true} duration={500} offset={-200} className={`about-link ${currentSection === 'about' ? 'active' : ''}`} onClick={() => setShowNav(false)}>
           <FontAwesomeIcon icon={faUser} color="var(--secondary-color)" />
         </Link>
         <Link to="experience" smooth={true} duration={500} className={`experience-link ${currentSection === 'experience' ? 'active' : ''}`} onClick={() => setShowNav(false)}>          
-        <FontAwesomeIcon icon={faBuilding} color="var(--secondary-color)" />
+          <FontAwesomeIcon icon={faBuilding} color="var(--secondary-color)" />
         </Link>
         <Link to="projects" smooth={true} duration={500} className={`projects-link ${currentSection === 'projects' ? 'active' : ''}`} onClick={() => setShowNav(false)}>
           <FontAwesomeIcon icon={faSuitcase} color="var(--secondary-color)" />
@@ -46,13 +70,13 @@ const Navbar = () => {
       </nav>
       <FontAwesomeIcon icon={faBars} color="var(--primary-color)" size="3x" className="hamburger-button" onClick={() => setShowNav(true)} />
 
-      <div className="arrow">
-            <div className="line1"></div>
-            <div className="line2"></div>
-            <div className="line3"></div>
-        </div>
-        <p className="Date">Mar 03, 2025</p>
-
+  <div className={`arrow ${isAtBottom ? "end-of-page" : ""}`}>
+    <div className="line1"></div>
+    <div className="line2"></div>
+    <div className="line3"></div>
+  </div>
+      
+      <p className="Date">{currentDate}</p>
     </div>
   );
 };
